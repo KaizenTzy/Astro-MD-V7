5d6115de74474aeaf7bdc269f1bb4c3801f8d82f
KaizenTzy <95485711+KaizenTzy@users.noreply.github.com>
2023-03-07 00:57:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ CAPTION: process.env.CAPTION === undefined ? '*Created by Athip*' : process.env.
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=Ji5FVAb#ZyU_1op-ww0i_mhMPzyQ5yZCoEUIJ15ZS2ZgyKWO9ZI' : process.env.SESSION_ID
 };
